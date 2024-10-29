cdf1ac9ff0b496682001f319434152284f2912f0
Thomas W <126Twalsh@frhsd.com>
2024-10-29 13:06:20 -0400

    Day 1

---
+++ js/java.js
@@ -0,0 +1,19 @@
+const d = new Date();
+let hour = d.getHours();
+hour = 2
+if(hour < 12){
+    document.getElementById ("Greetings").innerText = "Good Morning";
+    document.getElementById ("Picture").src = "img/hand-drawn-sunburst-clip-art-free-png.webp";
+    
+} else if (hour < 17){
+    document.getElementById ("Greetings").innerText = "Good Afternoon";
+    document.getElementById ("Picture").src = "img/4-clipart-sun-1.png";
+} else {
+    document.getElementById ("Greetings").innerText = "Good Night";
+    document.getElementById ("Picture").src = "img/purepng.com-moonmoonastronomical-bodyfifth-largest-natural-satellitenatural-satellitemoon-light-141152706663245bda.png";
+}
+
+//let greetingElement = document.getElementById ("Greetings");
+//console.log(greetingElement);
+
+
