3f19888e6a850bfdb4180b93f95498f05f8c1b3c
Thomas W <126Twalsh@frhsd.com>
2024-10-30 14:03:30 -0400

    Done

--- js/java.js
+++ js/java.js
@@ -1,16 +1,22 @@
 const d = new Date();
 let hour = d.getHours();
-hour = 2
+hour = 12
 if(hour < 12){
-    document.getElementById ("Greetings").innerText = "Good Morning";
-    document.getElementById ("Picture").src = "img/sun-rays-png-13.png";
+    document.querySelector (".words").innerText = "Good Morning";
+    document.querySelector ("#Picture").src = "img/sun-rays-png-13.png";
+    document.querySelector(".Night").classList.add("d-none");
+  document.querySelector(".Afternoon").classList.add("d-none");
     
 } else if (hour < 17){
-    document.getElementById ("Greetings").innerText = "Good Afternoon";
-    document.getElementById ("Picture").src = "img/4-clipart-sun-1.png";
+    document.querySelector (".words").innerText = "Good Afternoon";
+    document.querySelctor ("#Picture").src = "img/4-clipart-sun-1.png";
+    document.querySelector(".Night").classList.add("d-none");
+  document.querySelector(".morning").classList.add("d-none");
 } else {
-    document.getElementById ("Greetings").innerText = "Good Night";
-    document.getElementById ("Picture").src = "img/purepng.com-moonmoonastronomical-bodyfifth-largest-natural-satellitenatural-satellitemoon-light-141152706663245bda.png";
+    document.querySelctor (".words").innerText = "Good Night";
+    document.querySelctor ("#Picture").src = "img/purepng.com-moonmoonastronomical-bodyfifth-largest-natural-satellitenatural-satellitemoon-light-141152706663245bda.png";
+    document.querySelector(".morning").classList.add("d-none");
+  document.querySelector(".Afternoon").classList.add("d-none");
 }
 
 //let greetingElement = document.getElementById ("Greetings");
